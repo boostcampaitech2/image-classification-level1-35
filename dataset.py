9150b3857b8f70bdbb728e9777da363fe53abc47
KyungHyunLim <72729802+KyungHyunLim@users.noreply.github.com>
2021-08-24 22:27:56 +0900

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,218 @@
+import os
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+# 이미지 전체 경로 생성
+# train.csv의 path 컬럼 이용
+# load_augmentation은 아직 미완성이에요...
+def PathMaker(csv_paths, image_path, load_augmentation):
+    path_list = []  # 모든 이미지 경로들 저장할 리스트
+    label_list = [] # 모든 이미지 라벨들 저장할 리스트
+    
+    # train.csv 파일 읽어오기
+    img_dir = pd.read_csv(csv_paths)#"../input/data/train/train.csv")
+    
+    # paht 컬럼만 추출
+    img_dir_pathes = img_dir['path']
+    
+    # image_path = ../input/data/train/images" + img_dir['path']의 이미지 폴더 이름을 합치기
+    # 결과예시: ../input/data/train/images/[사람별 고유 폴더명]
+    img_dir_patheses = [os.path.join(image_path, img_d) for img_d in img_dir_pathes]
+   
+    # 풀 경로 만들기 위한 반복문/mask1
+    # 결과예시: path_list => ../input/data/train/images/[사람별 고유 폴더명]/mask1.jpg
+    #        label_list 에는 라벨들 저장
+    for f_name in img_dir_patheses:
+        for img_name in os.listdir(f_name):
+            if img_name[0] == '.': # .으로 시작하는 바이너리 파일 skip
+                continue
+            # 예시: ../input/data/train/images/[사람별 고유 폴더명]와 'mask1.jpg' 결합
+            maked_path = os.path.join(f_name, img_name)
+            
+            # GetLabel 함수로 라벨변환
+            label = GetLabel(maked_path)
+            
+            # 리스트에 넣어주기
+            path_list.append(maked_path)
+            label_list.append(label)
+    return path_list, label_list
+
+def GetLabel(path):
+        # 전체경로에서 마지막 부분 가져 오기 => [사람별 고유 폴더명], mask1.jpg (이부분)
+        items = path.split('/')[-2:]
+        
+        # [사람별 고유 폴더명] 정보 분해하기
+        number, sex, race, age = items[0].split('_')
+        
+        # mask1 부분 가져오기
+        mask_status = items[1].split('.')[0]
+
+        # class 조건에 따라 분류
+        # mask먼저 분류하면 incorrect_mask도 포함이 되서 incorrect 먼저 분류
+        if 'incorrect' in mask_status:
+            if sex == 'male':
+                if int(age) < 30:
+                    return 6
+                elif int(age) < 60:
+                    return 7
+                else:
+                    return 8
+            else:
+                if int(age) < 30:
+                    return 9
+                elif int(age) < 60:
+                    return 10
+                else:
+                    return 11
+        elif 'mask' in mask_status:
+            if sex == 'male':
+                if int(age) < 30:
+                    return 0
+                elif int(age) < 60:
+                    return 1
+                else:
+                    return 2
+            else:
+                if int(age) < 30:
+                    return 3
+                elif int(age) < 60:
+                    return 4
+                else:
+                    return 5
+        else:
+            if sex == 'male':
+                if int(age) < 30:
+                    return 12
+                elif int(age) < 60:
+                    return 13
+                else:
+                    return 14
+            else:
+                if int(age) < 30:
+                    return 15
+                elif int(age) < 60:
+                    return 16
+                else:
+                    return 17
+
+# Dataset 클래스 정의
+class TrainDataset_v2(Dataset):
+    # img_list = 훈련할 이미지 경로들
+    # label_list = 위 이미지에 맞는 라벨들
+    # transform = 전처리 정의
+    def __init__(self, img_list, label_list, transform):
+        self.X = img_list
+        self.y = label_list
+        self.transform = transform
+    
+    # 길이 반환
+    def __len__(self):
+        return len(self.y)
+    
+    # 데이터 반환
+    def __getitem__(self, index):
+        image = Image.open(self.X[index])
+        
+        # 이미지는 전처리 적용해서 반환
+        if self.transform:
+            image = self.transform(image)
+
+        return image, self.y[index]
+        
+
+# 봉인 이전버전    
+# class TrainDataset(Dataset):
+    # def __init__(self, csv_paths, img_paths, transform, load_augmentation):
+        # self.csv_paths = csv_paths
+        # self.img_paths = img_paths
+        # self.transform = transform
+        # self.load_augmentation = load_augmentation
+
+        # self.X_pathes = self.PathMaker() 
+        # self.y = [self.GetLabel(path) for path in self.X_pathes]
+        
+        # #self.features = pd.read_csv(csv_paths)
+
+    # def __len__(self):
+        # return len(self.X_pathes)
+
+    # def __getitem__(self, index):
+        # image = Image.open(self.X_pathes[index])
+        # #y = self.GetLabel(self.X_pathes[index])
+
+        # if self.transform:
+            # image = self.transform(image)
+
+        # return image, self.y[index]
+
+    # def GetFeatures(self, idx):
+        # return self.features.iloc[idx]
+
+    # def GetLabel(self, path):
+        # items = path.split('/')[-2:]
+        # number, sex, race, age = items[0].split('_')
+        # mask_status = items[1].split('.')[0]
+        # #print(number, sex, race, age, mask_status)
+
+        # if 'incorrect' in mask_status:
+            # if sex == 'male':
+                # if int(age) < 30:
+                    # return 6
+                # elif int(age) < 60:
+                    # return 7
+                # else:
+                    # return 8
+            # else:
+                # if int(age) < 30:
+                    # return 9
+                # elif int(age) < 60:
+                    # return 10
+                # else:
+                    # return 11
+        # elif 'mask' in mask_status:
+            # if sex == 'male':
+                # if int(age) < 30:
+                    # return 0
+                # elif int(age) < 60:
+                    # return 1
+                # else:
+                    # return 2
+            # else:
+                # if int(age) < 30:
+                    # return 3
+                # elif int(age) < 60:
+                    # return 4
+                # else:
+                    # return 5
+        # else:
+            # if sex == 'male':
+                # if int(age) < 30:
+                    # return 12
+                # elif int(age) < 60:
+                    # return 13
+                # else:
+                    # return 14
+            # else:
+                # if int(age) < 30:
+                    # return 15
+                # elif int(age) < 60:
+                    # return 16
+                # else:
+                    # return 17
+
+    # def PathMaker(self):
+        # path_list = []
+        # img_dir = pd.read_csv(self.csv_paths)#"../input/data/train/train.csv")
+        # img_dir_pathes = img_dir['path']
+        # img_dir_patheses = [os.path.join(self.img_paths, img_d) for img_d in img_dir_pathes]
+        # for f_name in img_dir_patheses:
+            # for img_name in os.listdir(f_name):
+                # if img_name[0] == '.':
+                    # continue
+                # if not self.load_augmentation and 'aug' in img_name :
+                    # continue
+                # #elif ('aug' in img_name and ('incorrect' in img_name or 'normal' in img_name)) or ('aug' not in img_name and 'mask' in img_name):
+                # path_list.append(os.path.join(f_name, img_name))
+        # return path_list       
---
+++ train.py
@@ -0,0 +1,92 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.transforms import Resize, ToTensor, Normalize
+from torch.utils.data import sampler
+
+# dataset.py 안에 있는거 전부 임포트
+from dataset import *
+
+# pip install -U scikit-learn 으로 설치 가능!
+# train_test_split => train, valid 셋 분할
+# StratifiedKfold => Cross validation
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    # 나중에 config로 바꿔줄 것들
+    k_fold_num = -1
+    load_augmentation = False
+    
+    train_csv_path = "../input/data/train/train.csv"
+    train_images_path = "../input/data/train/images/"
+    transform = transforms.Compose([
+        #transforms.CenterCrop(384),
+        Resize((384, 384), Image.BILINEAR),
+        ToTensor(),
+        Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
+    ])
+    
+    # device 설정
+    device = 'cuda' if  torch.cuda.is_available() else 'cpu'
+    print("-"*10, "Device info", "-"*10)
+    print(device)
+    print("-"*10, "-----------", "-"*10)
+
+    # 데이터 불러오기
+    print("Data Loading...")
+    img_list, y_list = PathMaker(train_csv_path, train_images_path, load_augmentation)
+
+    # Cross validation 안할때
+    if k_fold_num == -1:
+        # train, valid 데이터 분리
+        # train_test_split(X, y, 훈련크기(0.8 이면 80%), stratify = (클래스 별로 분할후 데이터 추출 => 원래 데이터의 클래스 분포와 유사하게 뽑아준다) )
+        # random_state는 원하는 숫자로 고정하시면 됩니다! 저는 42를 주로써서...
+        train_img, valid_img, train_y, valid_y = train_test_split(img_list, y_list, train_size=0.8, 
+                                shuffle=True, random_state=42, stratify=y_list)
+        
+        # dataset.py에서 구현한 dataset class로 훈련 데이터 정의
+        train_dataset = TrainDataset_v2(train_img, train_y, transform)
+        
+        # dataset.py에서 구현한 dataset class로 평가 데이터 정의
+        valid_dataset = TrainDataset_v2(valid_img, valid_y, transform)
+    
+        # DataLoader에 넣어주기
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+        print(f'Train_data: {len(train_dataset)}, Valid_data:{len(valid_dataset)}')
+        print("Data Loading... Success!!")
+        
+        # 구현 예정인 친구들...
+        print("Train Start!!")
+        result, best_model = train(train_loader, valid_loader)
+    # Cross validation 할때
+    else:
+        # K개의 corss validation 준비
+        kf = StratifiedKFold(n_splits=k_fold_num, random_state=42, shuffle=True)
+        
+        print(f'{k_fold_num} cross validation strat...')
+        
+        # kf가 랜덤으로 섞어서 추출해 index들을 반환
+        for fold_index, (train_idx, valid_idx) in enumerate(kf.split(img_list, y_list), 1):
+            print(f'{fold_index} fold start -')
+                
+            # index로 array 나누기
+            train_list, train_label = img_list[train_idx], y_list[train_idx]
+            valid_list, valid_label = img_list[valid_idx], y_list[valid_idx]
+            
+            # dataset.py에서 구현한 dataset class로 훈련 데이터 정의
+            train_dataset = TrainDataset_v2(train_img, train_y, transform)
+            
+            # dataset.py에서 구현한 dataset class로 평가 데이터 정의
+            valid_dataset = TrainDataset_v2(valid_img, valid_y, transform)
+            
+            # DataLoader에 넣어주기
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+            
+            # 구현 예정인 친구들...
+            print("Train Start!!")
+            result, best_model = train(train_loader, valid_loader)
+            
+            
+        
